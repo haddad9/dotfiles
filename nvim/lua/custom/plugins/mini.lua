34bf60bf4766e1fc03103983ce5eb996efd15485
haddad9 <emha.haddad@gmail.com>
2025-01-19 17:59:54 +0700

    mini.files nvim config
    Signed-off-by: haddad9 <emha.haddad@gmail.com>

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -9,6 +9,43 @@ return { -- Collection of various small independent plugins/modules
     --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
     require('mini.pairs').setup()
+    require('mini.files').setup {
+      options = {
+        use_as_default_explorer = false,
+      },
+      mappings = {
+        close = '<ESC>',
+        go_in_plus = '<CR>', -- close explorer when open
+        synchronize = 's',
+        go_out_plus = 'h', -- default H
+      },
+    }
+
+    -- File manager update height
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesWindowUpdate',
+      callback = function(args)
+        local config = vim.api.nvim_win_get_config(args.data.win_id)
+
+        -- Ensure fixed height
+        config.height = 45
+
+        -- Ensure title padding
+        if config.title[#config.title][1] ~= ' ' then
+          table.insert(config.title, { ' ', 'NormalFloat' })
+        end
+        if config.title[1][1] ~= ' ' then
+          table.insert(config.title, 1, { ' ', 'NormalFloat' })
+        end
+
+        vim.api.nvim_win_set_config(args.data.win_id, config)
+      end,
+    })
+    vim.api.nvim_create_user_command('MiniFilesOpen', function()
+      MiniFiles.open(vim.fn.expand('#' .. vim.api.nvim_get_current_buf()), false)
+    end, {})
+    -- Map the command to keyboard bind
+    vim.keymap.set({ 'n' }, '<leader>b', '<cmd>MiniFilesOpen<CR>', { desc = 'Open Directory [b] ' })
 
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     --
